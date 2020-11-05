abbf23b1d964855d67a5a95fde8ffafa65fa846f
yusuf <yusuf@MacBook-Pro-2.local>
2020-11-05 14:01:30 +0300

    v2

--- react-ui/src/redux/store.js
+++ react-ui/src/redux/store.js
@@ -14,7 +14,7 @@ const enhancers = [applyMiddleware(...middlewares)];
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["auth", "myCards"],
+  whitelist: ["auth", "myCards","transactions"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
